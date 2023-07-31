a6d14a228589109a166006fca90b9a6260aac17d
blprohit <rohitblpprajapat@gmail.com>
2023-07-31 23:22:53 +0530

    working on api

--- app/api.py
+++ app/api.py
@@ -1,45 +1,152 @@
-from flask_restful import Api, Resource, fields, marshal_with, reqparse, abort
-from flask import Flask, request
-import werkzeug
-from model import *
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, fields, marshal_with
+from model import db, User, Product, Category
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///store.db'
+db.init_app(app)
 api = Api(app)
 
+# Define request parsers
+product_parser = reqparse.RequestParser()
+product_parser.add_argument('name', type=str, required=True, help='Name is required')
+product_parser.add_argument('MFD', type=str, required=True, help='MFD is required')
+product_parser.add_argument('EXP', type=str, required=True, help='EXP is required')
+product_parser.add_argument('rate', type=int, required=True, help='Rate is required')
+product_parser.add_argument('description', type=str, required=True, help='Description is required')
+product_parser.add_argument('image', type=str, required=True, help='Image is required')
+product_parser.add_argument('mimetype', type=str, required=True, help='Mimetype is required')
+product_parser.add_argument('quantity', type=int, required=True, help='Quantity is required')
 
-prod_parser = reqparse.RequestParser()
-prod_parser.add_argument(
-    'name', fields.String
-)
+category_parser = reqparse.RequestParser()
+category_parser.add_argument('name', type=str, required=True, help='Name is required')
 
+# Define response fields
+product_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'MFD': fields.String,
+    'EXP': fields.String,
+    'rate': fields.Integer,
+    'description': fields.String,
+    'image': fields.String,
+    'mimetype': fields.String,
+    'quantity': fields.Integer
+}
 
+category_fields = {
+    'id': fields.Integer,
+    'name': fields.String
+}
 
-
-
-
-
-def abort_if(id):
-    product = db.session.query(Product).filter(Product.id == id).first()
-    if product is None:
-        abort(404, message="Todo {} doesn't exists.".format(id))
-        
-        
-class Product(Resource):
+# Define API resources
+class ProductResource(Resource):
+    @marshal_with(product_fields)
     def get(self, id):
-        abort_if(id)
-        return (db.session.query(Product).filter(Product.id == id).first())
-        
+        product = Product.query.get(id)
+        if product:
+            return product
+        else:
+            return {'message': 'Product not found'}, 404
+
+    @marshal_with(product_fields)
     def put(self, id):
-        product = db.session.query(Product).filter(Product.id == id).first()
-        
-    
-    
+        args = product_parser.parse_args()
+        product = Product.query.get(id)
+        if product:
+            product.name = args['name']
+            product.MFD = args['MFD']
+            product.EXP = args['EXP']
+            product.rate = args['rate']
+            product.description = args['description']
+            product.image = args['image']
+            product.mimetype = args['mimetype']
+            product.quantity = args['quantity']
+            db.session.commit()
+            return product
+        else:
+            return {'message': 'Product not found'}, 404
 
-# api.add_resource("Product", '/<int: di>')
+    def delete(self, id):
+        product = Product.query.get(id)
+        if product:
+            db.session.delete(product)
+            db.session.commit()
+            return {'message': 'Product deleted'}
+        else:
+            return {'message': 'Product not found'}, 404
 
+class ProductListResource(Resource):
+    @marshal_with(product_fields)
+    def get(self):
+        products = Product.query.all()
+        return products
 
+    @marshal_with(product_fields)
+    def post(self):
+        args = product_parser.parse_args()
+        product = Product(
+            name=args['name'],
+            MFD=args['MFD'],
+            EXP=args['EXP'],
+            rate=args['rate'],
+            description=args['description'],
+            image=args['image'],
+            mimetype=args['mimetype'],
+            quantity=args['quantity']
+        )
+        db.session.add(product)
+        db.session.commit()
+        return product, 201
 
-api.add_resource(TodoSimple, '/<string:todo_id>')
+class CategoryResource(Resource):
+    @marshal_with(category_fields)
+    def get(self, category_id):
+        category = Category.query.get(category_id)
+        if category:
+            return category
+        else:
+            return {'message': 'Category not found'}, 404
+
+    @marshal_with(category_fields)
+    def put(self, category_id):
+        args = category_parser.parse_args()
+        category = Category.query.get(category_id)
+        if category:
+            category.name = args['name']
+            db.session.commit()
+            return category
+        else:
+            return {'message': 'Category not found'}, 404
+
+    def delete(self, category_id):
+        category = Category.query.get(category_id)
+        if category:
+            db.session.delete(category)
+            db.session.commit()
+            return {'message': 'Category deleted'}
+        else:
+            return {'message': 'Category not found'}, 404
+
+class CategoryListResource(Resource):
+    @marshal_with(category_fields)
+    def get(self):
+        categories = Category.query.all()
+        return categories
+
+    @marshal_with(category_fields)
+    def post(self):
+        args = category_parser.parse_args()
+        category = Category(name=args['name'])
+        db.session.add(category)
+        db.session.commit()
+        return category, 201
+
+# Define API endpoints
+api.add_resource(ProductResource, '/products/<int:id>')
+api.add_resource(ProductListResource, '/products')
+api.add_resource(CategoryResource, '/categories/<int:category_id>')
+api.add_resource(CategoryListResource, '/categories')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/model.py
+++ app/model.py
@@ -5,9 +5,9 @@ db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key= True)
-    name = db.Column(db.String(), nullable=False)
-    username = db.Column(db.String(), nullable=False, unique=True)
-    password = db.Column(db.String(), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
     is_admin = db.Column(db.Boolean(), default=False)
     
     @property
@@ -18,7 +18,7 @@ class User(db.Model, UserMixin):
     
 class Product(db.Model):
     id = db.Column(db.Integer(), primary_key= True)
-    name = db.Column(db.String(), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
     MFD = db.Column(db.String())
     EXP = db.Column(db.String())
     rate = db.Column(db.Integer(), nullable=False)
@@ -26,6 +26,8 @@ class Product(db.Model):
     image = db.Column(db.Text, unique = True, nullable=False)
     mimetype = db.Column(db.Text, nullable=False)
     quantity = db.Column(db.Integer(), nullable=False)
+    category_id = db.Column(db.Integer(), db.ForeignKey('category.id'), nullable = False)
+    catagory = db.relationship('Category', backref=db.backref('products', lazy=True))
     
 class Category(db.Model):
     id = db.Column(db.Integer(), primary_key = True)
